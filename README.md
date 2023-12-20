eeb915e3ae4ffb5d36ce1bceaffd63956581cca9
张洪Heo <qewan@foxmail.com>
2023-12-20 18:42:42 +0800

    支持显示需要回复的内容

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Wechat-Official-Account-Web
+
  微信公众号的关注界面
  
 [预览效果](https://blog.zhheo.com/wechatOA/)
+
+## 参数
+
+可以在url中携带参数。例如
+
+```
+http://www.example.com?replyText=欢迎关注
+```
+
+### replyText
+
+用于显示关注公众号时应该回复的内容参数
--- main.js
+++ main.js
@@ -6,3 +6,32 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 1;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  // 获取URL参数
+  const urlParams = new URLSearchParams(window.location.search);
+  const replyText = urlParams.get('replyText');
+
+  // 检测是否有replyText参数
+  if (replyText) {
+      // 如果有参数，则更新wechat-need-reply-text的内容
+      document.getElementById('wechat-need-reply-text').textContent = replyText;
+  } else {
+      // 如果没有参数，则隐藏wechat-need-reply部分
+      document.getElementById('wechat-need-reply').style.display = 'none';
+  }
+  
+  document.getElementById('wechat-need-reply-copybtn').addEventListener('click', function() {
+      var textToCopy = document.getElementById('wechat-need-reply-text').innerText;
+      navigator.clipboard.writeText(textToCopy).then(function() {
+          console.log('Text copied to clipboard');
+          // Change the text and color of wechat-need-reply-back
+          var replyBackElement = document.getElementById('wechat-need-reply-back');
+          replyBackElement.innerText = '复制成功';
+          replyBackElement.style.color = 'green';
+      })
+      .catch(function(err) {
+          console.error('Could not copy text: ', err);
+      });
+  });
+});
