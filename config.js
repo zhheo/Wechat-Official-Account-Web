0b7cef22cd8183f3a821360fd5c58642ea3a1791
张洪Heo <qewan@foxmail.com>
2023-12-22 18:05:14 +0800

    优化文档，支持文本修改

---
+++ config.js
@@ -0,0 +1,16 @@
+const oa_name = "张洪Heo";
+const oa_description = "分享设计与科技生活";
+const web_beian = "京ICP备19051325号-2";
+
+function replaceText(elementId, newText) {
+  var element = document.getElementById(elementId);
+  if (element) {
+      element.innerText = newText;
+  } else {
+      console.log("Element with ID '" + elementId + "' not found.");
+  }
+}
+
+replaceText('wechatOA-name', oa_name);
+replaceText('wechatOA-description', oa_description)
+replaceText('web-beian', web_beian)
